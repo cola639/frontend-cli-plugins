f4d5496a2598354a769aa9be124e2dd0f6280bd9
cola639 <337507950@qq.com>
2024-01-09 10:49:45 +0800

    update: react h5 template

---
+++ react-h5-template/template/src/constant.ts
@@ -0,0 +1,8 @@
+export const LAYOUT_CONST = {
+  VERTICAL_LAYOUT: 'vertical',
+  HORIZONTAL_LAYOUT: 'horizontal',
+  DEFAULT_DRAWER: 'default',
+  MINI_DRAWER: 'mini-drawer'
+}
+
+export const TENCENT_APPKEY = 'EWOBZ-Q74RZ-WRBXM-TZF4M-WH3N6-TBB74'
